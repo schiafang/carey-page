deb1efd49b59c5015d4752bbe1718ff3cc479bd9
carey <schiafang@gmail.com>
2020-10-13 18:45:03 +0800

    feat: fetch github repo

--- src/views/Repo.vue
+++ src/views/Repo.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="repo">
-    <h1>This is an repo page</h1>
+    <div v-for="repo in repos" :key="repo.id">
+      <a :href="repo.url" target="_blank">{{ repo.title }}</a>
+      <p>
+        {{ repo.description }}
+      </p>
+    </div>
   </div>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      repos: []
+    }
+  },
+  created() {
+    this.fetchGithubApi()
+  },
+  methods: {
+    async fetchGithubApi() {
+      try {
+        const res = await axios.get('https://api.github.com/users/schiafang/repos')
+        this.repos = res.data.map(i => ({
+          id: i.id,
+          url: i.html_url,
+          title: i.name,
+          description: i.description || 'No description'
+        }))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
