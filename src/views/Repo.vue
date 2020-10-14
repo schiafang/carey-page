823f08775dbdfdc747d078ee200a48ae8b24fc5b
carey <schiafang@gmail.com>
2020-10-14 19:06:51 +0800

    feat: add throttle optimize

--- src/views/Repo.vue
+++ src/views/Repo.vue
@@ -2,17 +2,22 @@
   <div class="container">
     <div v-for="(repo, index) in repos" :key="index" class="repo-item">
       <div class="top">
-        <div>
-          <a :href="repo.url" target="_blank" class="repo-title"
-            >{{ repo.title }} <span class="repo-title-tip"></span
-          ></a>
-          <a
-            v-if="repo.homepage"
-            :href="repo.homepage"
-            target="_blank"
-            class="repo-demo"
-            >Demo</a
-          >
+        <div class="title-demo">
+          <div>
+            <a :href="repo.url" target="_blank" class="title"
+              >{{ repo.title }} <span class="title-tip"></span
+            ></a>
+          </div>
+
+          <div>
+            <a
+              v-if="repo.homepage"
+              :href="repo.homepage"
+              target="_blank"
+              class="demo"
+              >Demo</a
+            >
+          </div>
         </div>
         <span class="date">
           {{ repo.date }}
@@ -31,10 +36,7 @@ import moment from 'moment'
 export default {
   data() {
     return {
-      repos: [],
-      intersectionObserver: null,
-      firstItem: '',
-      lastItem: '',
+      repos: []
     }
   },
   created() {
@@ -42,12 +44,37 @@ export default {
   },
   mounted() {
     const container = document.querySelector('.container')
-    container.addEventListener('scroll', () => {
-      if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
-        let temp = this.repos
-        this.repos = [...temp, ...temp]
+
+    function throttle(fn, timeout) {
+      let last, timer
+
+      return function () {
+        const context = this
+        const args = arguments
+        const now = + new Date()
+
+        if (last && now < last + timeout) {
+          clearTimeout(timer)
+          timer = setTimeout(() => {
+            last = now
+            fn.apply(context, args)
+          }, timeout)
+        } else {
+          last = now
+          fn.apply(context, args)
+        }
       }
-    })
+    }
+
+    const that = this
+    function handleScroll() {
+      if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+        let temp = that.repos
+        that.repos = [...temp, ...temp]
+      }
+    }
+
+    container.addEventListener('scroll', throttle(handleScroll, 80))
   },
   methods: {
     async fetchGithubApi() {
@@ -79,69 +106,121 @@ export default {
   margin: 0 15px;
   height: calc(100vh - 100px);
   overflow-y: scroll;
-  padding: 0 30px;
+  padding: 0 15px;
 }
 
 .repo-item {
-  height: 50px;
   display: flex;
   flex-direction: column;
-  padding: 30px 0;
+  padding: 15px 0;
 
   .top {
     display: flex;
-    align-items: flex-end;
-    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    text-align: right;
+  }
+
+  .title-demo {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .title {
+    text-decoration: none;
+    font-size: 0.8rem;
+    margin-right: 5px;
+    padding: 3px 5px;
+    color: #fff;
+    background-color: #6e4555;
+    position: relative;
+
+    &:hover {
+      opacity: 0.5;
+
+      .title-tip::after {
+        opacity: 1;
+        top: -20px;
+      }
+    }
+  }
+
+  .demo {
+    font-size: 0.8rem;
+    text-decoration: none;
+    color: #6e4555;
+    font-weight: 500;
+
+    &:hover {
+      opacity: 0.5;
+    }
   }
 
   .date {
     color: #aaa;
-    font-size: 0.8rem;
+    font-size: 0.7rem;
     font-weight: 500;
+    margin: 5px 0;
   }
 
   .description {
-    padding: 15px 0;
+    padding: 5px 0;
+    font-size: 0.8rem;
   }
 }
 
-.repo-title {
-  text-decoration: none;
-  font-size: 1rem;
-  margin-right: 15px;
-  padding: 5px 15px;
-  color: #fff;
-  background-color: #6e4555;
-  position: relative;
-
-  &:hover {
-    opacity: 0.5;
-
-    .repo-title-tip::after {
-      opacity: 1;
-      top: -20px;
-    }
-  }
-}
-
-.repo-title-tip::after {
-  content: 'Repository';
-  font-size: 0.9rem;
-  opacity: 0;
+.title-tip {
   position: absolute;
-  top: 0;
-  right: 0;
-  color: #000;
-  transition: all 0.2s ease-in-out;
+
+  &::after {
+    content: 'Repository';
+    font-size: 0.9rem;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: #000;
+    transition: all 0.2s ease-in-out;
+  }
 }
 
-.repo-demo {
-  text-decoration: none;
-  color: #6e4555;
-  font-weight: 500;
+@media screen and (min-width: 400px) {
+  .container {
+    padding: 0 30px;
+  }
 
-  &:hover {
-    opacity: 0.5;
+  .repo-item {
+    .top {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-end;
+      justify-content: space-between;
+    }
+
+    .title-demo {
+      width: auto;
+    }
+
+    .title {
+      font-size: 1rem;
+      padding: 5px 15px;
+    }
+
+    .demo {
+      font-size: 1rem;
+    }
+
+    .date {
+      margin: 0;
+    }
+
+    .description {
+      font-size: 0.9rem;
+    }
   }
 }
 
